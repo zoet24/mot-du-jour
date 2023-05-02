f00e477f47b5999cccd743746979fb517e2ef276
Zoe <zt12542@my.bristol.ac.uk>
2023-05-02 23:13:24 +0200

    Add modal class to DeleteModal component

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -12,13 +12,10 @@ const DeleteModal: React.FC<DeleteModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div
-        className="absolute inset-0 bg-black opacity-50"
-        onClick={onClose}
-      ></div>
-      <div className="absolute bg-white px-8 py-4 space-y-2 rounded-lg border border-grey">
-        <h2 className="text-lg text-center">Delete Word</h2>
+    <div className="modal">
+      <div className="modal-overlay" onClick={onClose}></div>
+      <div className="modal-body">
+        <h2 className="text-lg">Delete Word</h2>
         <p>Are you sure you want to delete this word?</p>
         <div className="space-x-4 text-center">
           <button className="btn" onClick={onConfirm}>
